62f665295b5ef4eacf5b2bc0a55742a2e5027a19
Vaibhav Nikas <march4vaibhav@gmail.com>
2022-09-17 23:44:45 +0530

    setup static files and extract styles and scripts

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(expressLayouts);
 
+app.use(express.static('./assets'));
+
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
 app.use('/', require('./routes/index'));
 
 app.listen(port, function(err){
