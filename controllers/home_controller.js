0b3d4dbf869f541ae0d5e7019b581e5e981a45de
Vaibhav Nikas <march4vaibhav@gmail.com>
2022-09-17 22:51:36 +0530

    setup view engine

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,3 @@
 module.exports.home = function(req, res){
-    return res.send('<h1>Home</h1>');
+    return res.render('home');
 }
--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.use('/', require('./routes/index'));
 
 app.listen(port, function(err){
