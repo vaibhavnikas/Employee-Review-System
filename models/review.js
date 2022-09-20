304b378b31cccab25af2c9c8095f4eb7929ad533
Vaibhav Nikas <march4vaibhav@gmail.com>
2022-09-20 21:34:25 +0530

    create schema for review model

---
+++ models/review.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+    from_employee: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Employee'
+    },
+    to_employee: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Employee'
+    },
+    status: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true
+    },
+    feedback: {
+        type: String,
+        required: true
+    }
+});
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
