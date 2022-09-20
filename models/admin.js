525a81f02da805a2cf5bcebc23406c92005b372c
Vaibhav Nikas <march4vaibhav@gmail.com>
2022-09-20 21:35:51 +0530

    create schema for admin model

---
+++ models/admin.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const adminSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    assigned_reviews: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
+});
+
+const Admin = mongoose.model('Admin', adminSchema);
+
+module.exports = Admin;
