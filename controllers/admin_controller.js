48f0c9ec1ffb63f128791a40d3a7c6f377d5f30a
Vaibhav Nikas <march4vaibhav@gmail.com>
2022-11-16 18:13:55 +0530

    completed manage_employees.ejs

--- controllers/admin_controller.js
+++ controllers/admin_controller.js
@@ -1,11 +1,17 @@
+const Employee = require('../models/employee');
+
 module.exports.home = function(req, res){
     return res.render('admin_view',{
         title: 'Home | Admin'
     });
 }
 
-module.exports.manageEmployees = function(req, res){
+module.exports.manageEmployees = async function(req, res){
+
+    const employees = await Employee.find();
+
     return res.render('manage_employees',{
-        title: 'Manage Employees | Admin'
+        title: 'Manage Employees | Admin',
+        employees: employees
     })
 }
